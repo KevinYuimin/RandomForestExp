e371aa6162a60b6ce48a93ff068bbad49f6a33d4
Kevin Hsu <kevhsu@amazon.com>
2022-04-10 23:15:48 +0800

    feat: add config file.

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+ENDPOINT = os.environ.get('AWS_ENDPOINT')
+PATH_TO_CERTIFICATE = "certs/certificate.pem.crt"
+PATH_TO_PRIVATE_KEY = "certs/private.pem.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "certs/AmazonRootCA1.pem"
+TOPIC = "test/testing"
+SUB_TOPIC = "myTopic"
+CLIENT_ID = "kevhsu_test"
+
+if not ENDPOINT:
+    raise ValueError("AWS IOT ENDPOINT Not Specified")
